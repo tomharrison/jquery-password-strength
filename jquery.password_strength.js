340ad78fac55580e3f8765c8523492ee6f6f579a
Tom Harrison <tharrison@hugeinc.com>
2013-01-23 12:31:23 -0500

    Sane code formatting.
    - One statement per line.
    - Consistent spacing.
    - Consistent curly braces.
    - Remove one-liner control structures.

--- jquery.password_strength.js
+++ jquery.password_strength.js
@@ -36,7 +36,7 @@
  *
  */
 
-(function($){ 
+(function ($) { 
 	$.fn.shortPass = 'Too short';
 	$.fn.badPass = 'Weak';
 	$.fn.goodPass = 'Good';
@@ -44,7 +44,7 @@
 	$.fn.samePassword = 'Username and Password identical.';
 	$.fn.resultStyle = "";
 
-	 $.fn.passStrength = function(options) {  
+	 $.fn.passStrength = function(options) {
 		var defaults = {
 			shortPass: 		"shortPass",	//optional
 			badPass:		"badPass",		//optional
@@ -60,34 +60,35 @@
 		return this.each(function() { 
 			 var obj = $(this);
 
-			$(obj).unbind().keyup(function()
-			{
+			$(obj).unbind().keyup(function () {
+				var results = $.fn.teststrength($(this).val(), $(opts.userid).val(), opts);
 
-				var results = $.fn.teststrength($(this).val(),$(opts.userid).val(),opts);
-
-				if(opts.messageloc === 1)
-				{
+				if (opts.messageloc === 1) {
 					$(this).next("." + opts.baseStyle).remove();
 					$(this).after("<span class=\""+opts.baseStyle+"\"><span></span></span>");
 					$(this).next("." + opts.baseStyle).addClass($(this).resultStyle).find("span").text(results);
-				}
-				else
-				{
+				} else {
 					$(this).prev("." + opts.baseStyle).remove();
 					$(this).before("<span class=\""+opts.baseStyle+"\"><span></span></span>");
 					$(this).prev("." + opts.baseStyle).addClass($(this).resultStyle).find("span").text(results);
 				}
 			});
 
-			//FUNCTIONS
-			$.fn.teststrength = function(password,username,option){
-				var score = 0; 
+			// FUNCTIONS
+			$.fn.teststrength = function(password, username, option) {
+				var score = 0;
 
 				//password < 4
-				if (password.length < 4 ) { this.resultStyle =  option.shortPass;return $(this).shortPass; }
+				if (password.length < 4) {
+					this.resultStyle =  option.shortPass;
+					return $(this).shortPass;
+				}
 
 				//password == user name
-				if (password.toLowerCase()==username.toLowerCase()){this.resultStyle = option.badPass;return $(this).samePassword;}
+				if (password.toLowerCase()==username.toLowerCase()) {
+					this.resultStyle = option.badPass;
+					return $(this).samePassword;
+				}
 
 				//password length
 				score += password.length * 4;
@@ -97,34 +98,58 @@
 				score += ( $.fn.checkRepetition(4,password).length - password.length ) * 1;
 
 				//password has 3 numbers
-				if (password.match(/(.*[0-9].*[0-9].*[0-9])/)){ score += 5;} 
+				if (password.match(/(.*[0-9].*[0-9].*[0-9])/)) {
+					score += 5;
+				}
 
 				//password has 2 symbols
-				if (password.match(/(.*[!,@,#,$,%,^,&,*,?,_,~].*[!,@,#,$,%,^,&,*,?,_,~])/)){ score += 5 ;}
+				if (password.match(/(.*[!,@,#,$,%,^,&,*,?,_,~].*[!,@,#,$,%,^,&,*,?,_,~])/)) {
+					score += 5;
+				}
 
 				//password has Upper and Lower chars
-				if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)){  score += 10;} 
+				if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) {
+					score += 10;
+				}
 
 				//password has number and chars
-				if (password.match(/([a-zA-Z])/) && password.match(/([0-9])/)){  score += 15;} 
-				//
+				if (password.match(/([a-zA-Z])/) && password.match(/([0-9])/)) {
+					score += 15;
+				}
+
 				//password has number and symbol
-				if (password.match(/([!,@,#,$,%,^,&,*,?,_,~])/) && password.match(/([0-9])/)){  score += 15;} 
+				if (password.match(/([!,@,#,$,%,^,&,*,?,_,~])/) && password.match(/([0-9])/)) {
+					score += 15;
+				}
 
 				//password has char and symbol
-				if (password.match(/([!,@,#,$,%,^,&,*,?,_,~])/) && password.match(/([a-zA-Z])/)){score += 15;}
+				if (password.match(/([!,@,#,$,%,^,&,*,?,_,~])/) && password.match(/([a-zA-Z])/)) {
+					score += 15;
+				}
 
 				//password is just a numbers or chars
-				if (password.match(/^\w+$/) || password.match(/^\d+$/) ){ score -= 10;}
+				if (password.match(/^\w+$/) || password.match(/^\d+$/) ) {
+					score -= 10;
+				}
 
 				//verifying 0 < score < 100
-				if ( score < 0 ){score = 0;} 
-				if ( score > 100 ){  score = 100;} 
+				if (score < 0) {
+					score = 0;
+				}
+				if (score > 100) {
+					score = 100;
+				}
 
-				if (score < 34 ){ this.resultStyle = option.badPass; return $(this).badPass;} 
-				if (score < 68 ){ this.resultStyle = option.goodPass;return $(this).goodPass;}
+				if (score < 34 ) {
+					this.resultStyle = option.badPass;
+					return $(this).badPass;
+				}
+				if (score < 68) {
+					this.resultStyle = option.goodPass;
+					return $(this).goodPass;
+				}
 
-				this.resultStyle= option.strongPass;
+				this.resultStyle = option.strongPass;
 				return $(this).strongPass;
 			};
 		});
@@ -134,19 +159,19 @@
 
 $.fn.checkRepetition = function(pLen,str) {
 	var res = "";
-	for (var i=0; i<str.length ; i++ )
-	{
+	for (var i=0; i<str.length ; i++ ) {
 		var repeated=true;
 
-		for (var j=0;j < pLen && (j+i+pLen) < str.length;j++){
+		for (var j=0;j < pLen && (j+i+pLen) < str.length;j++) {
 			repeated=repeated && (str.charAt(j+i)==str.charAt(j+i+pLen));
 		}
-		if (j<pLen){repeated=false;}
+		if (j<pLen) {
+			repeated=false;
+		}
 		if (repeated) {
 			i+=pLen-1;
 			repeated=false;
-		}
-		else {
+		} else {
 			res+=str.charAt(i);
 		}
 	}
